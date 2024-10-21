455614a9696168641c00efbc46d55f192711e6ab
defycab <love.bellinder@gmail.com>
2024-10-21 12:16:17 +0200

    sending props along the complicated way

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ const App = () => {
   return (
     <>
       <User2 />
-      {/* <User
+      <br />
+      <User
         h3style={{ color: "lightblue", fontWeight: "lighter" }}
+        imgstyle={{ height: 90, width: 90 , borderRadius: 50}}
         name="Love"
         age={45}
+        imageUrl="https://ca.slack-edge.com/TA01UCHBN-U07J8U0R0DD-24739eb0a367-512"
       >
         My name is Love and I am an aspiring developer
-      </User> */}
+      </User>
     </>
   );
 };
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -5,15 +5,25 @@ type UserProps = {
   name: string;
   age: number;
   children: ReactNode;
-  h3style: React.CSSProperties;
+  imageUrl: string;
+  h3style?: React.CSSProperties;
+  imgstyle?: React.CSSProperties;
 };
 
-export const User = ({ name, age, children, h3style }: UserProps) => {
+export const User = ({
+  name,
+  age,
+  children,
+  imageUrl,
+  h3style,
+  imgstyle,
+}: UserProps) => {
   return (
     <div className="container">
-      <h3 style={h3style}>Name:</h3>
+      <h3 style={h3style}></h3>
       {name}
-      <h3 style={h3style}>Age: </h3> {age}
+      <h3 style={h3style}></h3> {age}
+      <img style={imgstyle} src={imageUrl}></img>
       <p className="content"></p>
       {children}
     </div>
