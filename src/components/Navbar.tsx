2834dfb221d8eeb1649efb5fc6ed8fc68d31a001
defycab <love.bellinder@gmail.com>
2024-10-21 21:37:50 +0200

    setup routing

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <header>
+      <h1> Welcome to my routing site</h1>
+      <section className="button-container">
+        <button />
+        <button />
+        <button />
+      </section>
+    </header>
+  );
+};
