d72bb14ea371b958ac38ecff1fa09ffdfa899cbe
defycab <love.bellinder@gmail.com>
2024-10-21 10:23:38 +0200

    add returns in user component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,11 +1,24 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
-export const User = () => {
+type UserProps = {
+  name: string;
+  age: number;
+  image?: HTMLImageElement;
+  children: ReactNode;
+};
 
-    type userProps = {
-        name: string
-        age: number
-        image: HTMLImageElement
-        children: ReactNode
-    }
-}
+export const User = ({ name, age, image, children }: UserProps) => {
+  return (
+    <>
+      <h3>Name: </h3> {name}
+      <h3>Age: </h3> {age}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deserunt
+        consequuntur voluptate architecto, libero dolore odio ipsam error ut
+        corrupti doloremque quisquam. Quas aut officia accusantium eum rerum
+        delectus eveniet?
+      </p>{" "}
+      {children}
+    </>
+  );
+};
