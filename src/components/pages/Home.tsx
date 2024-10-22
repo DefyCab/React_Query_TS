64a3b4079b519a1c9041ab2c0d5381ff266410ba
defycab <love.bellinder@gmail.com>
2024-10-23 00:02:39 +0200

    adding reat query constant

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,43 @@
-import { useEffect } from "react"
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import nobelPrizeList from "../../fixtures/prizes.json";
 
 export const Home = () => {
+  const [winners, setWinners] = useState();
+  const prizes = nobelPrizeList.prizes;
 
-const [winners, setWinners] = useState()
+  const wait = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
 
+  const prizeQuery = useQuery({
+    queryKey: ["prizes"],
+    queryFn: () => wait(1000).then(() => [...prizes]),
+  });
 
-}
+  if (prizeQuery.isLoading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <ul>
+        {prizes.map((prize: any) => (
+          <li key={prize.id}>
+            <h3>
+              {prize.category.toUpperCase()} - {prize.year}
+            </h3>
+            <ul>
+              {prize.laureates.map((laureate: any) => (
+                <li
+                  style={{ fontSize: "large", fontStyle: "italic" }}
+                  key={laureate.id}
+                >
+                  {laureate.firstname} {laureate.surname} -{" "}
+                  {laureate.motivation}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
